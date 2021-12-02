babcbb07e00e23813be8657effaf8742fcb2153e
Mônica Sousa <88598905+momosousa@users.noreply.github.com>
2021-12-01 21:23:49 -0300

    Create mais-sobre-strings.js

---
+++ aula24/mais-sobre-strings.js
@@ -0,0 +1,7 @@
+// VER AULA NOVAMENTE
+let umaString2 = "Um \"texto\""; // Escapar o caractere ("") 
+console.log(umaString)
+
+let umaString3 = "Um \\texto"; // Escapar o caractere (\) 
+console.log(outraString)
+
