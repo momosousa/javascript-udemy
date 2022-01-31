b9b5c6a659113b7c892a56257356cc40d7f40ede
Mônica Sousa <88598905+momosousa@users.noreply.github.com>
2022-01-31 20:03:53 -0300

    Create script.js

---
+++ aula34/script.js
@@ -0,0 +1,28 @@
+function meuEscopo () {
+    const form = document.querySelector('.form');
+    const resultado = document.querySelector('.resultado');
+    const pessoas = [];
+
+    function recebeEventoForm (evento){
+        evento.preventDefault();
+        const nome = form.querySelector('.nome').value;
+        const sobrenome = form.querySelector('.sobrenome').value;
+        const peso = form.querySelector('.peso').value;
+        const altura = form.querySelector('.altura').value;
+
+        pessoas.push(
+            {
+                nome: nome,
+                sobrenome: sobrenome,
+                peso: peso,
+                altura: altura,
+            },
+        );
+
+        resultado.innerHTML += `<p>${nome} ${sobrenome} ${peso} ${altura}</p>`;
+        console.log(pessoas);
+    }
+
+    form.addEventListener('submit', recebeEventoForm);
+}
+meuEscopo();
